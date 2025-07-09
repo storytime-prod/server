b12f6d2e66738100829adb4a3297d11acf07d1fb
AdithyaPadmashali <adithya.p2001@gmail.com>
2025-07-09 23:54:50 +0530

    Add Schema design

--- app/models/story.py
+++ app/models/story.py
@@ -2,8 +2,15 @@ from sqlmodel import Field, SQLModel
 
 
 class Story(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    age: int | None = Field(default=None, index=True)
-    secret_name: str
+    id: int = Field(default=None, primary_key=True)
+    title: str = Field(index=True)
+    author: str | None = Field(default=None, index=True)
+    content: str
     genre: str | None = Field(default=None, index=True)
+    isRoot: bool = Field(default=True, index=True)
+    created_at: str | None = Field(default=None, index=True)  # ISO format
+    is_public: bool | None = Field(
+        default=True, index=True
+    )  # Whether the story is public or private
+    likes: int | None = Field(default=0, index=True)  # Number of likes for the story
+    tags: list[str] = Field(default=[], index=True)  # Tags associated with the story
